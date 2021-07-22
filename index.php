0216af8f428ef5c3ec5f6686d3ee959175a166d5
JoseJunior23 <juniorferreira1919@hotmail.com>
2021-07-21 21:36:08 -0300

    Exercicio de form em php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Soma</title>
+</head>
+
+<body>
+  <form action="soma.php" method="post">
+    <input type="number" name="num1" placeholder="Entre com o primeiro numero">
+    <input type="number" name="num2" placeholder="Entre com o segundo numero">
+    <button type="submit">Calcular</button>
+  </form>
+</body>
+
+</html>
